d1d1a1b3b1cdd07e61fe8cae5b95a2e082e9cb2d
pacmano1 <44065187+pacmano1@users.noreply.github.com>
2023-03-01 13:36:11 -0600

    Create createListofAttachments.js

---
+++ createListofAttachments.js
@@ -0,0 +1,21 @@
+// Smarter Code courtesy of Tony Germano (agermano) from Mirth Slack / Forums.
+
+var attachmentsList = getAttachments().toArray()
+    .map((attachment,i) =>
+        new AttachmentEntry('file_prefix_' + i + '.pdf', attachment.getContentString(), attachment.getType())
+         
+
+         
+// Dumber code:
+
+var attachment_list = getAttachmentIds()
+var attachmentsList = Lists.list()
+for (var i = 0; i < attachment_list.length; i++) {
+	var attachment_entry = new AttachmentEntry();
+	var attachmentId = attachment_list.get(i)
+	var attachment = getAttachment(attachmentId, false)
+	attachment_entry.setMimeType(attachment.getType());
+	attachment_entry.setContent(attachment.getContentString()) 
+	attachment_entry.setName('Some Name: ' + i + '.pdf');
+	attachmentsList.add(attachment_entry);
+}
