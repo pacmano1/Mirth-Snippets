08dce4cd53467fb27e79bddfbbd4c10b4b82dea8
pacmano1 <44065187+pacmano1@users.noreply.github.com>
2023-03-27 15:55:48 -0500

    Create epicJWTforBackend.js

---
+++ epicJWTforBackend.js
@@ -0,0 +1,56 @@
+// Add to code templates https://raw.githubusercontent.com/kjur/jsrsasign/master/jsrsasign-all-min.js
+// IMPORTANT In this code template you must add this near the top because this library can be used with browsers.
+//
+// var navigator = {}  // fake the fact we have no browser
+// var window = {}     //
+
+// On Epic dev portal, you will need to upload your public key (their documentation is clear on generating your required rsa keys) and retreive your ClientID
+
+// Transformer code:
+
+// time
+const iat = Math.floor(Date.now() / 1000);
+
+// Set up the JWT header and payload
+var header =
+    {
+        "alg": "RS256",
+        "typ": "JWT"
+    };
+
+var privateKey = $cfg('jwt_secret') + ''  // our private key is in $cfg, again Epic portal is clear on generating this
+
+var payload =
+    {
+        "iss": "<your client id from portal>",
+        "sub": "<same client id as above",
+        "aud": "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token",
+        "jti": "some UUID here",
+        "exp": iat + 299,
+        "iat": iat
+    }
+
+// remove         "nbf": 1583524102, from above
+// Generate the JWT
+
+var sHeader = JSON.stringify(header);
+var sPayload = JSON.stringify(payload);
+var sKey = KEYUTIL.getKey(privateKey);
+
+var jwt = KJUR.jws.JWS.sign('RS256', sHeader, sPayload, sKey);
+
+$c('jwt', jwt);  // will be used on http call.
+
+// Using an http sender:
+
+// Query paramenters:
+
+// grant_type                               client_credentials
+// client_assertion_type                    urn:ietf:params:oauth:client-assertion-type:jwt-bearer
+// client_assertion                         ${jwt}
+
+// Content Type
+// x-www-form-urlencoded
+
+
+
