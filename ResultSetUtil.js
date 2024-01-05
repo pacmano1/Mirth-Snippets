21a51566e7024e6c2a74c68ccbea4bfa2be991bb
pacmano1 <44065187+pacmano1@users.noreply.github.com>
2024-01-05 12:09:31 -0600

    Create ResultSetUtil.js

---
+++ ResultSetUtil.js
@@ -0,0 +1,61 @@
+/**
+ * ResultSetUtil
+ * A utility for working with result sets from a database.
+ * 
+ * Courtesy of @agermano on Mirth Slack
+ */
+
+var ResultSetUtil = {
+    /**
+     * getRowLabels
+     * Retrieves the column labels from a result set's metadata.
+     * 
+     * @param {ResultSet} resultSet - The result set to extract labels from.
+     * @returns {string[]} An array of column labels.
+     */
+    getRowLabels: function getRowLabels(resultSet) {
+        var labels = [];
+        var meta = resultSet.metaData;
+        for (var i = 0; i < meta.columnCount; i++) {
+            labels.push(meta.getColumnLabel(i + 1));
+        }
+        return labels;
+    },
+
+    /**
+     * getRowAsJs
+     * Converts a row from a result set into a JavaScript object.
+     * 
+     * @param {ResultSet} resultSet - The result set containing the row data.
+     * @param {string[]} labels - An array of column labels.
+     * @returns {Object} A JavaScript object representing the row data.
+     */
+    getRowAsJs: function getRowAsJs(resultSet, labels) {
+        return labels.reduce(function (obj, label, i) {
+            if (resultSet.getObject(i + 1) != null) {
+                obj[label] = resultSet.getObject(i + 1).toString();
+            } else {
+                obj[label] = null;
+            }
+            return obj;
+        }, {});
+    },
+
+    /**
+     * getRowAsXml
+     * Converts a row from a result set into an XML element.
+     * 
+     * @param {ResultSet} resultSet - The result set containing the row data.
+     * @param {string[]} labels - An array of column labels.
+     * @returns {XML} An XML element representing the row data.
+     */
+    getRowAsXml: function getRowAsXml(resultSet, labels) {
+        return labels.reduce(function (obj, label, i) {
+            obj.appendChild(<{ label }>{resultSet.getObject(i + 1)}</{ label }>);
+            return obj;
+        }, <row />);
+    }
+}
+
+// Export the ResultSetUtil for usage in other modules if needed.
+module.exports = ResultSetUtil;
