630cb84b0bb68bc151026eda29008e603c552837
pacmano1 <44065187+pacmano1@users.noreply.github.com>
2024-03-10 08:17:05 -0600

    Update DBReadertoCSV.js

--- DBReadertoCSV.js
+++ DBReadertoCSV.js
@@ -1,8 +1,34 @@
 // Notes: Assumes you have aggregate results set to yes on the DB reader
 //        Do not use an outbound template with this solution.
 // Code courtesy of Tony Germano and Jon Bartels, original forum thread: https://forums.mirthproject.io/forum/mirth-connect/support/16869-
+/*
+ _____ _                 _        
+|_   _| |__   __ _ _ __ | | _____ 
+  | | | '_ \ / _` | '_ \| |/ / __|
+  | | | | | | (_| | | | |   <\__ \
+  |_| |_| |_|\__,_|_| |_|_|\_\___/
 
+ _____                 
+|_   _|__  _ __  _   _ 
+  | |/ _ \| '_ \| | | |
+  | | (_) | | | | |_| |
+  |_|\___/|_| |_|\__, |
+                 |___/
 
+                 _ 
+  __ _ _ __   __| |
+ / _` | '_ \ / _` |
+| (_| | | | | (_| |
+ \__,_|_| |_|\__,_|
+                   
+     _             
+    | | ___  _ __  
+ _  | |/ _ \| '_ \ 
+| |_| | (_) | | | |
+ \___/ \___/|_| |_|
+                   
+*/
+     
 // Set the root element name to 'delimited' (optional but mentioned)
 msg.setName('delimited');
 
